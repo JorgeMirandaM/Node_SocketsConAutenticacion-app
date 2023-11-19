03b7796cfde1999d7ac86a2aa28a8b86018fc61b
JorgeMirandaM <jorge100099@hotmail.com>
2023-11-19 13:19:30 -0600

    validate the JWT - Service

--- controllers/auth.js
+++ controllers/auth.js
@@ -100,7 +100,21 @@ const googleSignIn = async (req, res) => {
 
 }
 
+const renovarToken =async (req,res)=>{
+
+    const {usuario} = req;
+
+    //Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+        usuario,
+        token
+    })
+}
+
 module.exports = {
     login,
-    googleSignIn
+    googleSignIn,
+    renovarToken
 }
