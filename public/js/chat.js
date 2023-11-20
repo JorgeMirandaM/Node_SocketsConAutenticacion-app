1c5b644fa65cf5c919e52e670b4ecf8f0f9dc10d
JorgeMirandaM <jorge100099@hotmail.com>
2023-11-19 21:06:13 -0600

    chat design implemented

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,6 +7,12 @@ const url = (window.location.hostname.includes('localhost'))
 let usuario = null;
 let socket = null;
 
+const txtUid= document.querySelector('#txtUid');
+const txtMensajes= document.querySelector('#txtMensajes');
+const ulUsuarios= document.querySelector('#ulUsuarios');
+const ulMensajes= document.querySelector('#ulMensajes');
+const btnSalir= document.querySelector('#btnSalir');
+
 
 //Validar el token del localstorage
 const validarJWT = async () => {
@@ -36,10 +42,30 @@ const validarJWT = async () => {
 
 const conectarSocket= async ()=>{
 
-    const socket=io({
+    socket=io({
         'extraHeaders':{
             'x-token':localStorage.getItem('token')
         }
+    });
+
+    socket.on('connect',()=>{
+        console.log('Sockets online');
+    });
+
+    socket.on('disconnect',()=>{
+        console.log('Sockets offline');
+    });
+
+    socket.on('recibir-mensajes',()=>{
+
+    });
+
+    socket.on('usuarios-activos',()=>{
+
+    });
+
+    socket.on('mensaje-privado',()=>{
+
     });
 }
 
